8a99867fc05e7db534bf8bfca0db06b1aa00d3de
Ondrej Certik <ondrej@certik.cz>
2009-03-28 15:18:26 -0700

    This should work on Mac Os X

--- ncpus.c
+++ ncpus.c
@@ -1,10 +1,11 @@
-#include "unistd.h"
+#include "sys/sysctl.h"
 #include "stdio.h"
 
 int main()
 {
-    int ncpus;
-    ncpus = sysconf(_SC_NPROCESSORS_ONLN);
+    int ncpus = 1;
+    size_t len = sizeof(ncpus);
+    sysctl((int[2]) {CTL_HW, HW_NCPU}, 2, &ncpus, &len, NULL, 0);
     printf("%d", ncpus);
     return 0;
 }
